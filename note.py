188c141c697ec3bec08023d1ffb81ae4be81ee76
AyushAgrawal <ayushagrawal@pop-os.localdomain>
2021-05-08 22:17:05 +0530

    Read proporty file propoerly

--- note.py
+++ note.py
@@ -5,31 +5,24 @@ import sys
 import os
 from datetime import date, time
 import subprocess as sp
-from jproperties import Properties
+import configparser
+
+
 
 def setup():
-    propoerty_file = "application.properties"
-    config = Properties()
-    _dir = os.path.dirname(os.path.abspath(__file__))
-    with open(_dir+"/" + propoerty_file, "rb") as _file:
-        config.load(_file)
-        global extention
-        global editor
-        global author
-        global MAIN_FOLDER_PATH 
-        MAIN_FOLDER_PATH = config.get("base.folder").data if(config.get('base.folder') is not None) else "/home/ayushagrawal/notes"
-        MAIN_FOLDER_PATH = "/home/ayushagrawal/notes"
-        extention = "."+config.get("file_type").data if(config.get('file_type') is not None) else ".md"
-        editor = config.get("editor").data if(config.get('editor') is not None) else "vim"
-        author = config.get("author").data if(config.get('author') is not None) else ""
+    config = configparser.RawConfigParser()
+    config.read('./application.properties')
+    details_dict = dict(config.items('SECTION_NAME'))
+    return details_dict
 
-def read_args():
+def read_args(details_dict):
     parser = argparse.ArgumentParser()
 
     subparser = parser.add_subparsers(dest="command")
 
     find_parser = subparser.add_parser("find", help="find a file in your main application")
     find_parser.add_argument("file_name", help="Name of the file")
+    find_parser.add_argument("-e", "--extention", help="Extention for note", default=details_dict['file_type'])
 
     
     save_parser = subparser.add_parser("save", help="save file/s in Git repo of your choice")
@@ -39,28 +32,28 @@ def read_args():
     open_parser = subparser.add_parser("open", help="Open file or create new if not exist.")
     open_parser.add_argument("file_name", help="file name to open")
     open_parser.add_argument("-b", "--book", help="Book for note", default="")
+    open_parser.add_argument("-e", "--extention", help="Extention for note", default=details_dict['file_type'])
     open_parser.add_argument("-t", "--topic", help="Topic for note", default="")
-    open_parser.add_argument("-a", "--author", help="Autor of the document", default="")
+    open_parser.add_argument("-a", "--author", help="Autor of the document", default=details_dict['author'])
 
     return parser.parse_args()
 
 
-def find_file(_file):
-    _file = _file+extention
-    for root, _, files in os.walk(MAIN_FOLDER_PATH):
+def find_file(_file, extention):
+    _file = _file+"."+extention
+    for root, _, files in os.walk(details_dict['base.folder']):
         if(_file in files):
             print(root + "/" +_file)
 
 
-def open_file_not_exist(_file, book, topic):
-    _dir = MAIN_FOLDER_PATH + "/" + book 
+def open_file_not_exist(_file, book, topic, author, extention):
+    _dir = details_dict['base.folder'] + "/" + book 
     
     if(not os.path.exists(_dir)):
         os.makedirs(_dir)
 
-    author = parsed.author if(parsed.author != "") else "Ayush Agrawal"
     os.chdir(_dir)
-    with open(_file+extention, 'w') as f:
+    with open(_file+"."+extention, 'w') as f:
         f.write("Date: " + str(date.today) + "\nAuthor: "+author+"\nBook: " + book + "\nTopic: "+topic + "\n\n\n---\n")
 
 def save_file():
@@ -68,27 +61,30 @@ def save_file():
     pass
 
 
-def open_file(_file):
-    _book = parsed.book
-    _topic  = parsed.topic
-
-    file_path = MAIN_FOLDER_PATH + "/" + _book + "/"+_file+extention
+def open_file(_file, _book, _topic, _author, _extention):
+    print(_author)
+    print(_extention)
+    file_path = MAIN_FOLDER_PATH + "/" + _book + "/"+_file+"."+_extention
     if(not os.path.exists(file_path)):
-        open_file_not_exist(_file, _book, _topic)
+        open_file_not_exist(_file, _book, _topic,_author,  _extention)
 
     sp.call(editor + " " +file_path, shell=True)
 
-def main():
+def main(parsed):
         if(parsed.command == "find"):
-            find_file(parsed.file_name)
+            find_file(parsed.file_name, parsed.extention)
         if(parsed.command == "open"):
-            open_file(parsed.file_name)
+            open_file(parsed.file_name, parsed.book, parsed.topic, parsed.author, parsed.extention)
         if(parsed.command == 'save'):
             save_file()
 
 
-parsed = read_args()
 
 if __name__ == "__main__":
-    setup()
-    main()
+    details_dict = setup()
+    global MAIN_FOLDER_PATH
+    global editor 
+    editor = details_dict['editor']
+    MAIN_FOLDER_PATH = details_dict['base.folder']
+    parsed = read_args(details_dict)
+    main(parsed)
--- note_creation.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/notes/$1
-cd ~/notes/$1
-vim $2.md
